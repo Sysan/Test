6f22ca56501556256cca2ebdf9099e2decf549d5
Sysan <shifanglei7@Gmail.com>
2015-09-05 21:55:04 +0800

    welcome

---
+++ today.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>today.php</title>
+	<meta charset="utf-8"/>
+</head>
+<body>
+	<p>
+		<?php
+			print "<b>Welcome to my home page<br><br>";
+			print "Today is : <br>";
+			print date("l, F jS");
+			print "<br>";
+		?>
+	</p>
+</body>
+</html>
